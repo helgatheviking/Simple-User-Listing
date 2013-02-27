a57b482c9ec545b369f2ee83c98e79a0fc62be0b
Kathy Darling <helgatheviking@gmail.com>
2013-02-27 10:46:40 -0500

    add ID to author div

--- templates/content-author.php
+++ templates/content-author.php
@@ -12,7 +12,7 @@ global $user;
 
 $user_info = get_userdata($user->ID);
 ?>
-<div class="author-block">
+<div id="user-<?php echo $user->ID; ?>" class="author-block">
 	<?php echo get_avatar( $user->ID, 90 ); ?>
 	<h2><a href="<?php echo get_author_posts_url($user->ID); ?>"><?php echo $user_info->display_name; ?></a> - <?php printf( __( '%s posts', 'simple-user-listing'), count_user_posts( $user->ID ) ); ?></h2>
 	<p><?php echo $user_info->description; ?></p>
