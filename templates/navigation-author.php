81261a59faab3b19102e2f3da7d127969cbf5bb5
Kathy Darling <helgatheviking@gmail.com>
2013-07-07 11:43:58 -0400

    re-work markup for WP_Pagenavi

--- templates/navigation-author.php
+++ templates/navigation-author.php
@@ -10,19 +10,28 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $simple_user_listing, $sul_users;
 
-// Only show the navigation if needed
-if ( $simple_user_listing->get_total_user_pages() > 1 ) : ?>
+if( function_exists( 'wp_pagenavi' ) ){
 
-<nav id="nav-single">
-	<h3 class="assistive-text"><?php _e('User navigation', 'simple-user-listing');?></h3>
+		wp_pagenavi(array('query' => $sul_users,
+								'type' => 'users' ) );
+} else {
 
-<?php if ( $previous_url = $simple_user_listing->get_previous_users_url() ) { ?>
-		<span class="nav-previous"><a rel="prev" href="<?php esc_attr_e( $previous_url ); ?>"><span class="meta-nav">&larr;</span> <?php _e('Previous', 'simple-user-listing');?></a></span>
+	// Only show the navigation if needed
+	if ( $simple_user_listing->get_total_user_pages() > 1 ) { ?>
+
+	<nav id="nav-single">
+		<h3 class="assistive-text"><?php _e('User navigation', 'simple-user-listing');?></h3>
+
+	<?php		if ( $previous_url = $simple_user_listing->get_previous_users_url() ) { ?>
+				<span class="nav-previous"><a rel="prev" href="<?php esc_attr_e( $previous_url ); ?>"><span class="meta-nav">&larr;</span> <?php _e('Previous', 'simple-user-listing');?></a></span>
+			<?php } ?>
+
+			<?php if ( $next_url = $simple_user_listing->get_next_users_url() ) { ?>
+				<span class="nav-next"><a rel="next" href="<?php esc_attr_e( $next_url ); ?>"><?php _e('Next', 'simple-user-listing');?> <span class="meta-nav">&rarr;</span></a></span>
+			<?php } ?>
+
+	</nav>
+
 	<?php } ?>
 
-	<?php if ( $next_url = $simple_user_listing->get_next_users_url() ) { ?>
-		<span class="nav-next"><a rel="next" href="<?php esc_attr_e( $next_url ); ?>"><?php _e('Next', 'simple-user-listing');?> <span class="meta-nav">&rarr;</span></a></span>
-	<?php } ?>
-</nav>
-
-<?php endif; ?>
+<?php } ?>
