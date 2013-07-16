47fa69d35097737d57acea03c824d90d31d9f7e6
Kathy Darling <helgatheviking@gmail.com>
2013-07-15 23:32:32 -0400

    maintain role on search

--- templates/content-author.php
+++ templates/content-author.php
@@ -14,10 +14,6 @@ $user_info = get_userdata($user->ID);
 $num_posts = count_user_posts ( $user->ID );
 ?>
 <div id="user-<?php echo $user->ID; ?>" class="author-block">
-	<?php echo get_avatar( $user->ID, 90 ); ?>
-	<h2><a href="<?php echo get_author_posts_url($user->ID); ?>"><?php echo $user_info->display_name; ?></a> - <?php printf( _n( '%s post', '%s posts', $num_posts, 'simple-user-listing' ), $num_posts ); ?>
-	</h2>
-	<p><?php echo $user_info->description; ?></p>
+	<p><?php echo $user_info->first_name . ' ' . $user_info->last_name; ?></p>
 
-	<p><a href="<?php echo get_author_posts_url($user->ID); ?> ">Read <?php echo $user_info->display_name; ?> posts</a></p>
 </div>
