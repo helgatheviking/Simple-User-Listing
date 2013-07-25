3a0f192a14fc9689958d9f3a42e799bc4336e510
Kathy Darling <helgatheviking@gmail.com>
2013-07-25 12:03:29 -0400

    version bump for template fix

--- simple-user-listing.php
+++ simple-user-listing.php
@@ -4,7 +4,7 @@ Plugin Name: Simple User Listing
 Plugin URI: http://wordpress.org/extend/plugins/simple-user-listing/
 Description: Create a simple shortcode to list our WordPress users.
 Author: Kathy Darling
-Version: 1.3.2
+Version: 1.3.3
 Author URI: http://kathyisawesome.com
 License: GPL2
 
