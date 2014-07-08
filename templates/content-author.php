6ede62cb2a2937138bad8ef9c7897a8c8c734681
Kathy Darling <helgatheviking@gmail.com>
2014-07-08 13:26:32 -0400

    update content-author.php to stop linking to authors that have no posts

--- templates/content-author.php
+++ templates/content-author.php
@@ -10,16 +10,30 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 global $user;
 
-$user_info = get_userdata($user->ID);
-$num_posts = count_user_posts ( $user->ID );
+$user_info = get_userdata( $user->ID );
+$num_posts = count_user_posts( $user->ID );
 ?>
 <div id="user-<?php echo $user->ID; ?>" class="author-block">
 	<?php echo get_avatar( $user->ID, 90 ); ?>
-	<h2><a href="<?php echo get_author_posts_url($user->ID); ?>"><?php echo $user_info->display_name; ?></a> - <?php printf( _nx( '%n post', '%n posts', $num_posts, 'number of posts', 'simple-user-listing' ), $num_posts ); ?>
+
+	<h2>
+		<?php if ( $num_posts > 0 ) { 
+
+			printf( '<a href="%s" title="%s">%s</a> <span class="post-count"><span class="hyphen">-</span> %s</span>', 
+
+				get_author_posts_url( $user->ID ),
+				sprintf( esc_attr__( 'Read posts by %s', 'simple-user-listing' ), $user_info->display_name ),
+				$user_info->display_name,
+				sprintf( _nx( '1 post', '%s posts', $num_posts, 'number of posts', 'simple-user-listing' ), $num_posts )
+
+			);
+
+		?>	
+	<?php } else {
+			echo $user_info->display_name;
+		} ?>
 	</h2>
+
 	<p><?php echo $user_info->description; ?></p>
 
-	<?php echo get_user_meta( $user->ID, 'last_name', true ) . ', ' . get_user_meta( $user->ID, 'first_name', true ); ?>
-
-	<p><a href="<?php echo get_author_posts_url($user->ID); ?> ">Read <?php echo $user_info->display_name; ?> posts</a></p>
 </div>
