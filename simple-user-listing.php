671c9ee1830537a980d1b89d0eadfff20739e3f6
Kathy Darling <helgatheviking@gmail.com>
2018-05-06 12:48:22 -0500

    move template functions to separate file

--- simple-user-listing.php
+++ simple-user-listing.php
@@ -93,6 +93,8 @@ if ( ! class_exists( 'Simple_User_Listing' ) ) {
 		 */
 		public function __construct() {
 
+			include_once( 'includes/simple-user-listing-template-functions.php' );
+
 			add_action( 'init', array( $this, 'load_text_domain' ) );
 			add_shortcode( 'userlist', array( $this, 'shortcode_callback' ) );
 			add_action( 'simple_user_listing_before_loop', array( $this, 'add_search' ) );
@@ -575,60 +577,3 @@ if ( ! class_exists( 'Simple_User_Listing' ) ) {
 // Launch the whole plugin.
 global $simple_user_listing;
 $simple_user_listing = Simple_User_Listing::get_instance();
-
-
-/**
- * Get template part
- *
- * @access public
- * @since 1.0
- * @param mixed $slug
- * @param string $name (default: '')
- * @return null
- */
-function sul_get_template_part( $slug, $name = '' ) {
-	global $simple_user_listing;
-	$template = '';
-
-	// Look in yourtheme/slug-name.php and yourtheme/simple-user-listing/slug-name.php
-	if ( $name ){
-		$template = locate_template( array ( "{$slug}-{$name}.php", "{$simple_user_listing->template_url()}{$slug}-{$name}.php" ) );
-	}
-	
-	if ( !$template && $name && file_exists( $simple_user_listing->plugin_path() . "/templates/{$slug}-{$name}.php" ) ){
-		$template = $simple_user_listing->plugin_path() . "/templates/{$slug}-{$name}.php";
-	}
-
-	// If template file doesn't exist, look in yourtheme/slug.php and yourtheme/simple_user_listing/slug.php
-	if ( !$template ){
-		$template = locate_template( array ( "{$slug}.php", "{$simple_user_listing->template_url()}{$slug}.php" ) );
-	}
-
-	// Allow 3rd party plugins to filter template file from their plugin.
-	$template = apply_filters( 'sul_get_template_part', $template, $slug, $name );
-
-	if ( $template ){
-		load_template( $template, false );
-	}
-
-}
-
-/**
- * Is User listing post/page?
- * Won't be true on archives
- *
- * @access public
- * @since 1.0
- * @return boolean
- */
-function is_user_listing(){
-	global $post;
-
-	$listing = false;
-
-	if( is_singular() && isset($post->post_content) && has_shortcode( $post->post_content, 'userlist' ) ) {
-		$listing = true;
-	}
-
-	return apply_filters( 'sul_is_user_listing', $listing );
-}
