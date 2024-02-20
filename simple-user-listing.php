9a4bdcfcabbc3f457fd41402a844078637dcd7f9
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-02-20 12:00:43 -0500

    Fix plugin header docblock

--- simple-user-listing.php
+++ simple-user-listing.php
@@ -1,32 +1,30 @@
 <?php
-/*
-Plugin Name: Simple User Listing
-Plugin URI: http://www.kathyisawesome.com/489/simple-user-listing/
-Description: Create a simple shortcode to list our WordPress users.
-Author: Kathy Darling
-Version: 1.9.3
-Author URI: http://kathyisawesome.com
-License: GPL2
-Text Domain: simple-user-listing
-
-Copyright 2015  Kathy Darling  (email: kathy.darling@gmail.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
-
-/*
- * adapted from: Cristian Antohe
+/**
+ * Plugin Name: Simple User Listing
+ * Plugin URI: http://www.kathyisawesome.com/489/simple-user-listing/
+ * Description: Create a simple shortcode to list our WordPress users.
+ * Author: Kathy Darling
+ * Version: 1.9.3
+ * Author URI: http://kathyisawesome.com
+ * License: GPL2
+ * Text Domain: simple-user-listing
+ * 
+ * Copyright 2015  Kathy Darling				
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Adapted from: Cristian Antohe
  * http://wp.smashingmagazine.com/2012/06/05/front-end-author-listing-user-search-wordpress/
  */
 
