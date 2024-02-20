dde90dab13463661ad91faa429747c9852eaa5ce
helgatheviking <507025+helgatheviking@users.noreply.github.com>
2024-02-20 14:56:20 -0500

    update copyright year

--- simple-user-listing.php
+++ simple-user-listing.php
@@ -9,7 +9,7 @@
  * License: GPL2
  * Text Domain: simple-user-listing
  * 
- * Copyright 2015  Kathy Darling				
+ * Copyright 2024  Kathy Darling				
  * 
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2, as
